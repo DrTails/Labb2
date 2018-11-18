9a9d4d898f3c6919638c77f6a365bedd573c4a53
danielmartinsson <e4g3f56f>
2018-11-18 23:15:25 +0100

    Finished G

---
+++ Labb2/Position.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace Labb2
+{
+    public class Position
+    {
+
+        int x;
+        int y;
+
+        public int X
+        {
+            get { return x; }
+            set { x = value < 0 ? 0 : value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value < 0 ? 0 : value; }
+        }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2));
+        }
+
+        public bool Equals(Position p)
+        {
+            return p.X == X && p.Y == Y;
+        }
+
+        public Position Clone()
+        {
+            return new Position(X, Y);
+        }
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")";
+        }
+
+        public static bool operator >(Position p1, Position p2)
+        {
+            return p1.Length() > p2.Length();
+        }
+
+        public static bool operator <(Position p1, Position p2)
+        {
+            return p1.Length() < p2.Length();
+        }
+
+        public static Position operator +(Position p1, Position p2)
+        {
+            return new Position(p1.X + p2.X, p1.Y + p2.Y);
+        }
+
+        public static Position operator -(Position p1, Position p2)
+        {
+            return new Position(p1.X - p2.X, p1.Y - p2.Y);
+        }
+
+        public static double operator %(Position p1, Position p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + (Math.Pow(p1.Y - p2.Y, 2)));
+        }
+
+    }
+}
---
+++ Labb2/SortedPosList.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Labb2
+{
+    public class SortedPosList
+    {
+
+        private List<Position> PositionsList { get; set; }
+
+        public SortedPosList()
+        {
+            PositionsList = new List<Position>();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", PositionsList);
+        }
+
+        public int Count()
+        {
+            return PositionsList.Count;
+        }
+
+        public void Add(Position pos)
+        {
+            PositionsList.Add(pos);
+            PositionsList = PositionsList.OrderBy(p => p.Length()).ToList();
+        }
+
+        public bool Remove(Position pos)
+        {
+            if (PositionsList.Contains(pos))
+            {
+                PositionsList.Remove(pos);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public SortedPosList Clone()
+        {
+            SortedPosList cloneList = new SortedPosList();
+            foreach (Position p in PositionsList)
+            {
+                cloneList.Add(p.Clone());
+            }
+            return cloneList;
+        }
+
+        public SortedPosList CircleContent(Position centerPos, double radius)
+        {
+            SortedPosList circleList = new SortedPosList();
+            foreach (Position p in PositionsList)
+            {
+                if ((Math.Pow(p.X - centerPos.X, 2) + (Math.Pow(p.Y - centerPos.Y, 2))) < Math.Pow(radius, 2))
+                {
+                    circleList.Add(p.Clone());
+                }
+            }
+            return circleList;
+        }
+
+        public static SortedPosList operator +(SortedPosList sp1, SortedPosList sp2)
+        {
+            SortedPosList combinedList = new SortedPosList();
+            foreach (Position p in sp1.PositionsList)
+            {
+                combinedList.Add(p.Clone());
+            }
+            foreach (Position p in sp2.PositionsList)
+            {
+                combinedList.Add(p.Clone());
+            }
+            return combinedList;
+        }
+
+    }
+}
